8d0d9ff1b13338f312b0608d15828c19dc717b00
Zach <ZachKalb@gmail.com>
2025-03-01 12:25:28 -0500

    V0.2.0 - Added additional functionality for bin clearing, bin stats, and running large duplicate removal jobs

--- main.py
+++ main.py
@@ -34,6 +34,9 @@ clr.AddReference(data.get("productLookupCatalog", "C:\\Program Files\\Redbox\\RE
 from Redbox.ProductLookupCatalog import Archive
         
 
+clr.AddReference("System.Collections")
+from System.Collections.Generic import Stack
+
 clr.AddReference(data.get("VistaDB", "C:\\Program Files\\Redbox\\REDS\\Kiosk Engine\\bin\\VistaDB.NET20"))
 # Import the VistaDB namespaces
 from VistaDB.Provider import VistaDBConnection
@@ -168,7 +171,6 @@ def waitForIdle(connect, interval=5, timeout=3600):
         result = getSchedulerStatus(connect)  # Call the function
         debug("result: ", result[1])
         if result[1] == 'idle':  # Check if response is good
-            print("Job Complete! Continuing...")
             break  # Exit loop if response is good
 
         elapsed_time = time.time() - start_time  # Calculate elapsed time
@@ -188,6 +190,91 @@ def getInventory(executable):
         print(f"Error: {e}")
     return result.returncode
 
+def executeCommand(connect,job,command):
+    try:
+        result = connect.ExecuteImmediate(command, job)
+    except Exception as e:
+        print(f"Error: {e}")
+    return result
+
+def executeServiceCommand(connect,command):
+    try:
+        result = connect.ExecuteServiceCommand(command)
+    except Exception as e:
+        print(f"Error: {e}")
+    return result
+
+def scheduleJob(connect,schedule,job,command,priority):
+    try:
+        beginJobs = GetAllJobs(connect)
+        result = connect.ScheduleJob(command, "Schedule Job", False, schedule, job)
+        endJobs = GetAllJobs(connect)
+
+        job.ID = getDifference(beginJobs,endJobs)
+        debug("Job ID: ", job.ID)
+        job.SetLabel("RDR Tool")
+        job.Pend()
+        commandResult = []
+        if waitForIdle(connect,1,60):
+            return 1
+        else:
+            return 0
+    except Exception as e:
+        print(f"Error: {e}")
+        return 0
+
+def getDiskInBin(connect,schedule,job):
+    scheduleJob(connect, schedule, job, "get-barcodes-in-bin", "Normal")
+    snInBinResult = []
+    stack = Stack[str]()
+    result, stack = job.GetStack(stack)
+    while stack.Count > 0:
+        tempStore1 = stack.Pop()
+        debug("tempstore1: ", tempStore1)
+        tempStore2 = tempStore1.split("|")
+        debug("tempstore 2: ", tempStore2)
+        snInBinResult.append(tempStore2[5])
+    return snInBinResult
+
+def adjustList(inputList ,limit):
+    try:
+        listlen = len(inputList)
+        if listlen <= limit:
+            debug("List is already under the limit")
+        elif listlen >= limit:
+            debug("List is over the limit, trimming list")
+            trimmedList = inputList[0:limit]
+            remainingItems = inputList[limit:]
+            debug("Trummed List Count: ", len(trimmedList))
+            debug("Remaining Items Count: ", len(remainingItems))
+    except Exception as e:
+        print(f"Error: {e}")
+    return trimmedList, remainingItems
+
+def startDuplicatesJob(connect, remove_List, schedule, job):
+    beginJobs = GetAllJobs(connect)
+
+    binDisks(connect,remove_List,schedule,job)
+
+    endJobs = GetAllJobs(connect)
+
+    job.ID = getDifference(beginJobs,endJobs)
+    debug("Job ID: ", job.ID)
+    job.SetLabel("RDR Tool: Remove Duplicates Disk(s)")
+    job.Pend()
+
+    binResult = waitForIdle(connect,5,3600)
+
+    if binResult:
+        print("Duplicates Removed!")
+        trashJob(connect, job.ID)
+        return 1
+
+    else:
+        print("Bin Job Timeout! Please check HAL Management Console for JOB ID: ")
+        print(job.ID)
+        return 0
+
 def debug(*args, **kwargs):
     if DEBUG:
         print("[DEBUG]:", *args, **kwargs)
@@ -205,72 +292,88 @@ remove_ListName = []
 beginJobs = []
 endJobs = []
 
-print("Welcome to the Kiosk Duplicate Binning Script, V0.1")
-print("By: Zach3697")
-print("")
-print("This script currently treats each type as a seperate title")
-print("and will keep a DVD, Blu-Ray, and 4K copy of each title.")
-print("A fututre version will address this soon and can be run")
-print("after this script has been used.")
-print("--------------------------------------------------------------")
-print("")
-print("Please select from the options below:")
-print("1. Just get stats on the nuber of duplicates in my kiosk (no binning)")
-print("2. Start the process of removing duplicates!")
-print("")
-user_choice = input("Enter your choice (1-2): ")
+exit = "Y"
+while exit == "Y" or exit == "y":
+    print("Welcome to the Kiosk Duplicate Binning Script, V0.1")
+    print("By: Zach3697")
+    print("")
+    print("This script currently treats each type as a seperate title")
+    print("and will keep a DVD, Blu-Ray, and 4K copy of each title.")
+    print("A fututre version will address this soon and can be run")
+    print("after this script has been used.")
+    print("--------------------------------------------------------------")
+    print("")
+    print("Please select from the options below:")
+    print("1. Just get stats on the nuber of duplicates in my kiosk (no binning)")
+    print("2. Start the process of removing duplicates!")
+    print("3. Check which disk(s) are in the bin")
+    print("4. Clear the Bin!")
+    print("")
+    user_choice = input("Enter your choice (1-4): ")
 
-print("Please Wait.....")
+    getInventory(data.get("HALUtilities", "HalUtilities.exe"))
 
-getInventory(data.get("HALUtilities", "HalUtilities.exe"))
+    non_empty_ids = extract_nonempty_ids(data.get("XML", "inventory.xml"))
 
-non_empty_ids = extract_nonempty_ids(data.get("XML", "inventory.xml"))
+    binCount = len(getDiskInBin(connection, schedule, job))
 
-for id in non_empty_ids:
-    if id != "UNKNOWN":
-        titleInfo = search(id)
-        record = title()
-        record.DiskSN = id
-        record.LongName = titleInfo['long_name']
-        record.ProductType = titleInfo['product_type_id']
-        record.SortName = titleInfo['sort_name']
-        record.titleID = titleInfo['product_id']
-        if record.titleID != 1139:
-            titleList.append(record)
- 
-for index,title in enumerate(titleList):
-    debug("Title: ", title.LongName)
-    if title.titleID in keep_List:
-        remove_List.append(title.DiskSN)
-        remove_ListName.append(title.LongName)
+    if user_choice == "1" or user_choice == "2":
+        print("Please Wait.....")
+        remainingList = []
+        for id in non_empty_ids:
+            if id != "UNKNOWN":
+                titleInfo = search(id)
+                record = title()
+                record.DiskSN = id
+                record.LongName = titleInfo['long_name']
+                record.ProductType = titleInfo['product_type_id']
+                record.SortName = titleInfo['sort_name']
+                record.titleID = titleInfo['product_id']
+                if record.titleID != 1139:
+                    titleList.append(record)
+        
+        for index,title in enumerate(titleList):
+            debug("Title: ", title.LongName)
+            if title.titleID in keep_List:
+                remove_List.append(title.DiskSN)
+                remove_ListName.append(title.LongName)
+            else:
+                keep_List.append(title.titleID)
+
+        print("Removing these titles: ", remove_ListName)
+        print("Which associates to these SNs: ", remove_List)
+        print("Total Number of Duplicates to be removed: ", len(remove_List))
+        print("Total Number of Disk(s) in the bin: ", binCount)
+        if len(remove_List) > 60-binCount:
+            print("Note: There is not enough space in the bin for all disks. Once filled,")
+            print("you will need to empty the bin, reset the bin, and start another duplicate removal job")
+            remainingList = remove_List[60-binCount:]
+            remove_List = remove_List[0:60-binCount]
+        print("")
+        if len(remove_List) != 0:
+            user_choice = input("Do you wish to remove these duplicates? (Y/N): ")
+            if user_choice == "Y" or user_choice == "y":
+                result = startDuplicatesJob(connection, remove_List, schedule, job)
+
+    elif user_choice == "3":
+        print("Checking Disk(s) in bin......")
+        result = scheduleJob(connection, schedule, job, "get-barcodes-in-bin", "Normal")
+        snInBinResult = []
+        stack = Stack[str]()
+        result, stack = job.GetStack(stack)
+        while stack.Count > 0:
+            tempStore1 = stack.Pop()
+            tempStore2 = tempStore1.split("|")
+            snInBinResult.append(tempStore2[5])  # "Third", "Second", "First" (LIFO order)
+        print(f"There are {len(snInBinResult)} disks in the bin. The SN(s) are shown below: ")
+        print(snInBinResult)
+
+    elif user_choice == "4":
+        print("Resetting bin......")
+        result = executeCommand(connection, job, "DUMPBIN RESETCOUNTERMOVE")
+        print("Bin Reset Complete! If you have more duplicates, please start another duplicate removal job.")
+
     else:
-        keep_List.append(title.titleID)
+        print("Invalid selection")
 
-print("Removing these titles: ", remove_ListName)
-print("Which associates to these SNs: ", remove_List)
-print("")
-user_choice = input("Do you wish to remove these duplicates? (Y/N): ")
-
-
-
-if user_choice == "2" or user_choice == "Y" or user_choice == "y":
-    beginJobs = GetAllJobs(connection)
-
-    binDisks(connection,remove_List,schedule,job)
-
-    endJobs = GetAllJobs(connection)
-
-    job.ID = getDifference(beginJobs,endJobs)
-    job.SetLabel("Remove Duplicates Disk(s)")
-    job.Pend()
-
-    binResult = waitForIdle(connection,5,3600)
-
-    if binResult:
-        print("Duplicates Removed!")
-        trashJob(connection, job.ID)
-
-    else:
-        print("Bin Job Timeout! Please check HAL Management Console for JOB ID: ")
-        print(job.ID)
-
+    exit = input("Return to main menu? (Y/N): ")
